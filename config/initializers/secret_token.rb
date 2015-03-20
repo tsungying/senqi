ab8829ff750dc81fe656ca5b70362c9de00342dd
Kenny zhou <ken65218@gmail.com>
2015-03-20 14:25:09 +0800

    first git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miracode::Application.config.secret_token = '173840e29dc300434b48f709599a3141823c0a11fe3350846435da00eab8a167ea207f0844fa22ba95234eb2d95a3528479f9add48c07a4e019a6b1d26c6be25'
