2c4fa34e93eb2bf6091ee18f49f70508deb7d9c5
Arisu Liu <tsungying721@gmail.com>
2015-04-01 20:11:11 +0800

    Modify event sort

--- app/controllers/event_categories_controller.rb
+++ app/controllers/event_categories_controller.rb
@@ -2,11 +2,11 @@ class EventCategoriesController < ApplicationController
 
   def index
   	@event_categories = EventCategory.all
-  	@events = Event.order("id desc").all
+  	@events = Event.order("publish_at desc").all
   end
 
   def show
   	@event_categories = EventCategory.all
-  	@events = EventCategory.find(params[:id]).events.order("id desc")
+  	@events = EventCategory.find(params[:id]).events.order("publish_at desc")
   end
 end
