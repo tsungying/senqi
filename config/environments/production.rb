fdbce5be294be3359138ebc07b660993e6517760
Arisu Liu <tsungying721@gmail.com>
2015-04-23 11:31:43 +0800

    Fix register error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,14 +65,15 @@ Miracode::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
+  host = "miracode.com"
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => 'miracode.com',
-    :user_name            => 'miracode.service@gmail.com',
-    :password             => ENV['MAIL_PASS'],
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
     :enable_starttls_auto => true,
     :authentication       => 'plain' 
   }  
