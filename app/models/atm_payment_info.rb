b4bb150f3c90417bbf703d5b28e0f6d9ffe5ee20
Arisu Liu <tsungying721@gmail.com>
2015-05-08 16:15:14 +0800

    Fix atm payment info expire_date error

--- app/models/atm_payment_info.rb
+++ app/models/atm_payment_info.rb
@@ -18,9 +18,11 @@ class AtmPaymentInfo < ActiveRecord::Base
 														trade_no: 					params_hash["TradeNo"],
 														bank_code: 					params_hash["BankCode"],
 														v_account: 					params_hash["vAccount"],
-														expire_date: 				params_hash["ExpireDate"],
+														expire_date: 				Date.parse(params_hash["ExpireDate"]).end_of_day,
 														rtn_code: 					params_hash["RtnCode"],
 														verify_mac: 				result )
+		# self.expire_date = expire_date.end_of_day
+
 		if self.rtn_code == 2 && self.verify_mac
 			Order.find_by_merchant_trade_no(self.merchant_trade_no).update_attributes(order_status_id: 5)
 		end
