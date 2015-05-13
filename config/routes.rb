e2164847969f0d565e771c229aa7cf80d86b3e50
Arisu Liu <tsungying721@gmail.com>
2015-05-13 18:22:49 +0800

    Build cart & top icon

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Rails.application.routes.draw do
     resources :company_profiles, :home_pages, :users, :orders
     resources :terms_of_services, :comments
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
