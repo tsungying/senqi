f9a98d1a18a9161c8b7664d740b3974a501b9fe8
Arisu Liu <tsungying721@gmail.com>
2015-05-22 17:17:05 +0800

    Fix sms content

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -23,7 +23,7 @@ class NotificationsController < ApplicationController
 			if settings.send_sms_to_ceo?
 		    @client = Twilio::REST::Client.new(ENV['TEST_TWILIO_ACCOUNT_SID'], ENV['TEST_TWILIO_AUTH_TOKEN'])
 		    phone = number_to_phone(settings.ceo_phone.to_i, country_code: 886) 
-		    body = settings.ceo_sms+"，消費金額為#{order.total}，付款時間：= #{ l(notification.payment_date, format: :long ) }"
+		    body = settings.ceo_sms+"，消費金額為#{order.total}，付款時間：#{ l(notification.payment_date, format: :long ) }"
 		    msg = @client.messages.create(from: '+15005550006', to: phone, body: body)
 		    Rails.logger.info msg.body			
 		  end
