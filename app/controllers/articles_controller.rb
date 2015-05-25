755b2c4f933155feb88ee0fbe78a055d7d9c20c6
Arisu Liu <tsungying721@gmail.com>
2015-05-25 17:19:58 +0800

    add blog & article pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,15 +2,16 @@ class ArticlesController < ApplicationController
 
   def index
 		@categories = BlogCategory.all
-		@comments = Comment.order("id desc").limit(10)  
+		@comments = Comment.order("id desc")
 		@articles = Article.search(params[:article_keyword])
   end
 
   def show
 		@categories = BlogCategory.all
-		@articles = BlogCategory.find(params[:blog_category_id]).articles.order("id desc")
+		articles = BlogCategory.find(params[:blog_category_id]).articles.order("id desc")
+		@latest_articles = articles.limit(10)
 		@article = Article.find(params[:id])
-		@comments = @article.comments.order("created_at desc")
+		@comments = @article.comments.order("created_at desc").page(params[:page]).per(10)
 		@comment = @article.comments.new
   end
 
--- app/controllers/blog_categories_controller.rb
+++ app/controllers/blog_categories_controller.rb
@@ -2,14 +2,20 @@ class BlogCategoriesController < ApplicationController
 
   def index
 		@categories = BlogCategory.all
-		@articles = Article.order("id desc").all
-		@comments = Comment.order("id desc").limit(10)
+		articles = Article.order("id desc")
+		@articles = articles.page(params[:page]).per(10)
+		@latest_articles = articles.limit(10)		
+
+		@comments = Comment.where(commentable_type: 'Article').order("id desc").limit(10)
 		
   end
 
   def show
   	@categories = BlogCategory.all
-		@articles = BlogCategory.find(params[:id]).articles.order("id desc")
-		@comments = Comment.where(:commentable_id => @articles.pluck(:id).uniq).order("id desc").limit(10)
+		articles = BlogCategory.find(params[:id]).articles.order("id desc")
+		@articles = articles.page(params[:page]).per(10)
+		@latest_articles = articles.limit(10)	
+
+		@comments = Comment.where(commentable_type: 'Article', :commentable_id => articles.pluck(:id).uniq).order("id desc").limit(10)
   end
 end
